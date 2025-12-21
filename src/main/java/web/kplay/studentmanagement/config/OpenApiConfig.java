a035a56f00a248c0235ed862f629b859e929b83f
Chanyeop <qkrcksduq1gh@naver.com>
2025-12-21 17:05:57 +0900

    feat : 임시저장

--- src/main/java/web/kplay/studentmanagement/config/OpenApiConfig.java
+++ src/main/java/web/kplay/studentmanagement/config/OpenApiConfig.java
@@ -1,41 +1,9 @@
 package web.kplay.studentmanagement.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI studentManagementOpenAPI() {
-        final String securitySchemeName = "Bearer Authentication";
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("학원 관리 시스템 API")
-                        .description("한국 학원을 위한 종합 관리 시스템 API 문서")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("K-Play Academy")
-                                .email("support@kplay.web"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                                .description("JWT 토큰을 입력하세요 (Bearer 접두사 불필요)")));
-    }
+    // SpringDoc 의존성 제거로 인해 임시 비활성화
+    // Spring Boot 4.0 호환 버전이 나오면 다시 활성화 예정
 }
